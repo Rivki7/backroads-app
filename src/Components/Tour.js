d4b5c4586d1b384292488940885ffa9a4587b60a
Rivki7 <rivkiii770@gmail.com>
2023-10-26 17:28:17 +0300

    before final

--- src/Components/Tour.js
+++ src/Components/Tour.js
@@ -1,44 +1,29 @@
-import Title from './Title';
-import { tours } from '../data';
-
-const Tour = () => {
+const Tour = ({ image, date, title, info, duration, location, cost }) => {
   return (
-    <section className="section" id="tours">
-      <Title title="featured" subTitle="tours" />
-
-      <div className="section-center featured-center">
-        {tours.map(
-          ({ id, image, date, title, info, duration, location, cost }) => {
-            return (
-              <article className="tour-card" key={id}>
-                <div className="tour-img-container">
-                  <img src={image} className="tour-img" alt="" />
-                  <p className="tour-date">{date}</p>
-                </div>
-                <div className="tour-info">
-                  <div className="tour-title">
-                    <h4>{title}</h4>
-                  </div>
-                  <p>{info}</p>
-                  <div className="tour-footer">
-                    <p>
-                      <span>
-                        <i className="fas fa-map"></i>
-                      </span>{' '}
-                      {location}
-                    </p>
-                    <p>
-                      {duration} {duration > 1 ? 'days' : 'day'}
-                    </p>
-                    <p>from ${cost}</p>
-                  </div>
-                </div>
-              </article>
-            );
-          }
-        )}
+    <article className="tour-card">
+      <div className="tour-img-container">
+        <img src={image} className="tour-img" alt="" />
+        <p className="tour-date">{date}</p>
       </div>
-    </section>
+      <div className="tour-info">
+        <div className="tour-title">
+          <h4>{title}</h4>
+        </div>
+        <p>{info}</p>
+        <div className="tour-footer">
+          <p>
+            <span>
+              <i className="fas fa-map"></i>
+            </span>{' '}
+            {location}
+          </p>
+          <p>
+            {duration} {duration > 1 ? 'days' : 'day'}
+          </p>
+          <p>from ${cost}</p>
+        </div>
+      </div>
+    </article>
   );
 };
 export default Tour;
